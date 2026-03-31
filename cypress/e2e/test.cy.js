6f2d2346c2b17d15199f9532075b0f1c6778f939
surya26-spec <sur24imt61l@gmail.com>
2026-03-31 09:40:46 +0530

    Update homepage test for case-insensitive check

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,16 +1,16 @@
 describe('Regression Test', () => {
 
   it('Loads homepage', () => {
-    cy.visit('/')   // uses baseUrl from config
+    cy.visit('/')   // uses baseUrl
   })
 
   it('Checks page content', () => {
     cy.get('body').should('be.visible')
 
-    // Strong validation (important)
-    cy.contains('Look').should('exist')
+    // More stable check (case-insensitive alternative)
+    cy.contains(/look/i).should('exist')
 
-    // Optional image check
+    // Image validation
     cy.get('img').should('have.length.greaterThan', 0)
   })
 
