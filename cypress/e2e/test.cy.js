23d2108b37751255b89f8e647a8945763437b620
surya26-spec <sur24imt61l@gmail.com>
2026-03-31 09:23:01 +0530

    Refactor homepage test for better validation
    Updated homepage visit method and enhanced content checks.

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,10 +1,17 @@
 describe('Regression Test', () => {
+
   it('Loads homepage', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')   // uses baseUrl from config
   })
 
   it('Checks page content', () => {
-    cy.get('body').should('exist')
-    cy.get('img').should('exist')
+    cy.get('body').should('be.visible')
+
+    // Strong validation (important)
+    cy.contains('Look').should('exist')
+
+    // Optional image check
+    cy.get('img').should('have.length.greaterThan', 0)
   })
+
 })
