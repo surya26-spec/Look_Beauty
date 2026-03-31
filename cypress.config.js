452a783716ab4258c6b801cf60a1897b7d145363
surya26-spec <sur24imt61l@gmail.com>
2026-03-31 11:20:50 +0530

    Update supportFile path in Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    supportFile: false,   // 🔥 safest in OCI
+    supportFile: 'cypress/support/e2e.js',   // ✅ FIXED
     specPattern: 'cypress/e2e/**/*.cy.js'
   }
 })
