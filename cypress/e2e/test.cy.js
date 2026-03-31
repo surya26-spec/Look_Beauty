1d264cd93d8e124c0aa8dbdc8c2223819fb97ea3
surya26-spec <sur24imt61l@gmail.com>
2026-03-31 10:19:55 +0530

    Update case-insensitive check to case-sensitive

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -2,16 +2,11 @@ describe('Regression Test', () => {
 
   it('Loads homepage', () => {
     cy.visit('/')
-    cy.title().should('not.be.empty')
   })
 
   it('Checks page content', () => {
     cy.get('body').should('be.visible')
-
-    // Case-insensitive check
-    cy.contains(/look/i).should('exist')
-
-    // Check images exist
+    cy.contains('look').should('exist')
     cy.get('img').should('have.length.greaterThan', 0)
   })
 
