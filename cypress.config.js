05ce1d16778630597885e034277e00cd1588d6bf
surya26-spec <sur24imt61l@gmail.com>
2026-03-31 11:04:08 +0530

    Add supportFile and specPattern to Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -3,5 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-  },
+    supportFile: 'cypress/support/e2e.js',   // ✅ FIX
+    specPattern: 'cypress/e2e/**/*.cy.js'
+  }
 })
